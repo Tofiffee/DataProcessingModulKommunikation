fa12fa21ac19dc0eda694b3acac3096578bfcb24
Laurenz Fedotoff <laurenz.fedotoff@gmail.com>
2022-06-14 17:53:08 +0200

    added statistics and finished chirp class

--- ChirpDataClass.py
+++ ChirpDataClass.py
@@ -15,6 +15,19 @@ class ChirpData():
         self.Chirp2 = self.ReadChirpData(self.sheetID, self.sheetName2)
         self.Chirp3 = self.ReadChirpData(self.sheetID, self.sheetName3)
         self.temperature = self.Basedata['Temperatur'].dropna().iloc[0]
+        self.syllable1 = self.CalcChirpComponentTime('time_puls1')
+        self.syllable2 = self.CalcChirpComponentTime('time_puls2')
+        self.syllable3 = self.CalcChirpComponentTime('time_puls3')
+        self.syllable4 = self.CalcChirpComponentTime('time_puls4')
+        self.interTime1 = self.CalcChirpComponentTime('Ruheintervall1')
+        self.interTime2 = self.CalcChirpComponentTime('Ruheintervall2')
+        self.interTime3 = self.CalcChirpComponentTime('Ruheintervall3')
+        self.refraektaertime = self.CalcChirpComponentTime('Refraektaerzeit')
+        self.Frequency1 = self.CalcFrequency(1)
+        self.Frequency2 = self.CalcFrequency(2)
+        self.Frequency3 = self.CalcFrequency(3)
+        self.Frequency4 = self.CalcFrequency(4)
+        self.Chirptimes = self.syllable1 + self.syllable2 + self.syllable3 + self.syllable4 + self.interTime1 + self.interTime2 + self.interTime3
 
     def ReadBaseData(self, url): 
         """
@@ -33,42 +46,38 @@ class ChirpData():
 
         return df
 
-    def CalcChirpTime(self):
-        df_time1 = self.Chirp1[['time_puls1', 'time_puls2', 'time_puls3', 'time_puls4']]
-        df_time2 = self.Chirp2[['time_puls1', 'time_puls2', 'time_puls3', 'time_puls4']]
-        df_time3 = self.Chirp3[['time_puls1', 'time_puls2', 'time_puls3', 'time_puls4']]
+    def CalcChirpComponentTime(self, columnName):
+        df_time1 = self.Chirp1[columnName]
+        df_time2 = self.Chirp2[columnName]
+        df_time3 = self.Chirp3[columnName]
 
-        index = ['time_puls1', 'time_puls2', 'time_puls3', 'time_puls4']
         dfs = [df_time1, df_time2, df_time3]
 
-        silible_lenghts = []
-
-        for i in index:
-            timevalues = []
+        timevalues = []
             
-            for df in dfs:
-                time_df = df[i].dropna()
-                timevalue = time_df.iloc[-1]
-                timevalues.append(timevalue)
-            
-            timevalues = np.array(timevalues)
-            meanvalue = timevalues.mean()
-            
-            silible_lenghts.append(meanvalue)
+        for df in dfs:
+            df = df.dropna()
+            timevalue = df.iloc[-1]
+            timevalues.append(timevalue)
         
-        return silible_lenghts
+        timevalues = np.array(timevalues)
+
+        return timevalues
+
+    def CalcFrequency(self, pulsNumber):
+        frequ = self.Basedata[[f'Chirp1_puls{pulsNumber}', f'Chirp2_puls{pulsNumber}', f'Chirp3_puls{pulsNumber}']].iloc[0]
+        frequ = frequ.apply(lambda x: int(x.split(',')[0]))
+        frequ = frequ.to_numpy()
+
+        return frequ
 
 
 listURL = [
     'https://docs.google.com/spreadsheets/d/13Mmcw54O7G_LruX0nXUkFv-C3Y5NNvO6XzAQnhEzeqc/edit#gid=0',
-    'https://docs.google.com/spreadsheets/d/11YsRYABukndOhe8loN3_4oKDjRfV6mJo/edit#gid=1173836825',
-    "https://docs.google.com/spreadsheets/d/178msiAEd50V1jYQf805meYiqwlQJYb3W/edit#gid=1498647522"
     ]
 
 listSheetID = [
     '13Mmcw54O7G_LruX0nXUkFv-C3Y5NNvO6XzAQnhEzeqc',
-    '11YsRYABukndOhe8loN3_4oKDjRfV6mJo',
-    '178msiAEd50V1jYQf805meYiqwlQJYb3W'
     ]
 
 ChirpObjectList = []
@@ -77,24 +86,30 @@ for i, j in zip(listURL, listSheetID):
     ChirpObject = ChirpData(i, j)
     ChirpObjectList.append(ChirpObject)
 
-temps = []
-silb1 = []
-silb2 = []
-silb3 = []
-silb4 = []
+print(ChirpObjectList[0].syllable1)
+print(ChirpObjectList[0].interTime1)
+print(ChirpObjectList[0].refraektaertime)
+print(ChirpObjectList[0].Basedata)
+print(ChirpObjectList[0].Frequency3)
+print(ChirpObjectList[0].Chirptimes)
+# temps = []
+# silb1 = []
+# silb2 = []
+# silb3 = []
+# silb4 = []
 
-for i in ChirpObjectList:
-    silbList = i.CalcChirpTime()
-    silb1.append(silbList[0])
-    silb2.append(silbList[1])
-    silb3.append(silbList[2])
-    silb4.append(silbList[3])
-    temps.append(i.temperature)
+# for i in ChirpObjectList:
+#     silbList = i.CalcSyllableTime()
+#     silb1.append(silbList[0])
+#     silb2.append(silbList[1])
+#     silb3.append(silbList[2])
+#     silb4.append(silbList[3])
+#     temps.append(i.temperature)
 
-print(temps)
-print(silb1)
-print(silb2)
-print(silb3)
-print(silb4)
+# print(temps)
+# print(silb1)
+# print(silb2)
+# print(silb3)
+# print(silb4)
 
-ScatterPlotSilibleTime(temps, silb1, silb2, silb3, silb4)
+# ScatterPlotSilibleTime(temps, silb1, silb2, silb3, silb4)
